eaa27b02a759bcbba91097e505dff88db3fda4f2
Mark Chesney <mches@users.noreply.github.com>
2023-07-21 21:01:19 -0700

    Add connectionCatalog precondition

---
+++ markitect-liquibase-parent/markitect-liquibase-core/src/main/java/dev/markitect/liquibase/precondition/ConnectionCatalogPrecondition.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2023 Markitect
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.markitect.liquibase.precondition;
+
+import static dev.markitect.liquibase.base.Preconditions.checkNotNull;
+
+import dev.markitect.liquibase.base.Nullable;
+import java.util.Objects;
+import liquibase.changelog.ChangeSet;
+import liquibase.changelog.DatabaseChangeLog;
+import liquibase.changelog.visitor.ChangeExecListener;
+import liquibase.database.Database;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.PreconditionErrorException;
+import liquibase.exception.PreconditionFailedException;
+import liquibase.exception.ValidationErrors;
+import liquibase.exception.Warnings;
+import liquibase.precondition.AbstractPrecondition;
+import liquibase.structure.core.Catalog;
+
+public class ConnectionCatalogPrecondition extends AbstractPrecondition {
+  private @Nullable String catalogName;
+
+  public @Nullable String getCatalogName() {
+    return catalogName;
+  }
+
+  public void setCatalogName(@Nullable String catalogName) {
+    this.catalogName = catalogName;
+  }
+
+  @Override
+  public Warnings warn(Database database) {
+    checkNotNull(database);
+    return new Warnings();
+  }
+
+  @Override
+  public ValidationErrors validate(Database database) {
+    checkNotNull(database);
+    ValidationErrors errors = new ValidationErrors();
+    errors.checkRequiredField("catalogName", catalogName);
+    return errors;
+  }
+
+  @Override
+  public void check(
+      Database database,
+      @Nullable DatabaseChangeLog changeLog,
+      @Nullable ChangeSet changeSet,
+      @Nullable ChangeExecListener changeExecListener)
+      throws PreconditionFailedException, PreconditionErrorException {
+    checkNotNull(database);
+    try {
+      String connectionCatalogName = database.getConnection().getCatalog();
+      if (!Objects.equals(
+          connectionCatalogName, database.correctObjectName(catalogName, Catalog.class))) {
+        throw new PreconditionFailedException(
+            String.format(
+                "Connection catalog precondition failed: expected %s, was %s",
+                catalogName, connectionCatalogName),
+            changeLog,
+            this);
+      }
+    } catch (DatabaseException | RuntimeException e) {
+      throw new PreconditionErrorException(e, changeLog, this);
+    }
+  }
+
+  @Override
+  public String getSerializedObjectNamespace() {
+    return GENERIC_CHANGELOG_EXTENSION_NAMESPACE;
+  }
+
+  @Override
+  public String getName() {
+    return "connectionCatalog";
+  }
+}
---
+++ markitect-liquibase-parent/markitect-liquibase-core/src/test/java/dev/markitect/liquibase/precondition/ConnectionCatalogPreconditionTests.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2023 Markitect
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.markitect.liquibase.precondition;
+
+import static liquibase.serializer.LiquibaseSerializable.GENERIC_CHANGELOG_EXTENSION_NAMESPACE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.InstanceOfAssertFactories.list;
+import static org.assertj.core.api.InstanceOfAssertFactories.type;
+
+import dev.markitect.liquibase.base.Nullable;
+import dev.markitect.liquibase.database.DatabaseBuilder;
+import dev.markitect.liquibase.database.mssql.MarkitectMssqlDatabase;
+import java.util.List;
+import liquibase.database.Database;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.PreconditionErrorException;
+import liquibase.exception.PreconditionFailedException;
+import liquibase.precondition.FailedPrecondition;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junitpioneer.jupiter.json.JsonSource;
+
+class ConnectionCatalogPreconditionTests {
+  @Test
+  void test() {
+    // when
+    var precondition = new ConnectionCatalogPrecondition();
+    precondition.setCatalogName("cat1");
+
+    // then
+    assertThat(precondition.getCatalogName()).isEqualTo("cat1");
+    assertThat(precondition.getSerializedObjectNamespace())
+        .isEqualTo(GENERIC_CHANGELOG_EXTENSION_NAMESPACE);
+    assertThat(precondition.getSerializedObjectName()).isEqualTo("connectionCatalog");
+  }
+
+  @ParameterizedTest
+  @JsonSource(
+      """
+      [
+        {
+          databaseClass: 'liquibase.database.core.MSSQLDatabase',
+          expectedMessages: []
+        }
+      ]
+      """)
+  void warn(Class<? extends Database> databaseClass, List<String> expectedMessages)
+      throws Exception {
+    // given
+    var precondition = new ConnectionCatalogPrecondition();
+    try (var database = DatabaseBuilder.of(databaseClass).build()) {
+
+      // when
+      var warnings = precondition.warn(database);
+
+      // then
+      assertThat(warnings.hasWarnings()).isEqualTo(!expectedMessages.isEmpty());
+      assertThat(warnings.getMessages()).containsExactlyInAnyOrderElementsOf(expectedMessages);
+    }
+  }
+
+  @ParameterizedTest
+  @JsonSource(
+      """
+      [
+        {
+          databaseClass: 'liquibase.database.core.MSSQLDatabase',
+          expectedErrorMessages: [
+            'catalogName is required'
+          ]
+        },
+        {
+          databaseClass: 'liquibase.database.core.MSSQLDatabase',
+          catalogName: 'cat1',
+          expectedErrorMessages: []
+        }
+      ]
+      """)
+  void validate(
+      Class<? extends Database> databaseClass,
+      @Nullable String catalogName,
+      List<String> expectedErrorMessages)
+      throws Exception {
+    // given
+    var precondition = new ConnectionCatalogPrecondition();
+    precondition.setCatalogName(catalogName);
+    try (var database = DatabaseBuilder.of(databaseClass).build()) {
+
+      // when
+      var errors = precondition.validate(database);
+
+      // then
+      assertThat(errors.getErrorMessages())
+          .containsExactlyInAnyOrderElementsOf(expectedErrorMessages);
+      assertThat(errors.hasErrors()).isEqualTo(!expectedErrorMessages.isEmpty());
+    }
+  }
+
+  @ParameterizedTest
+  @CsvSource(
+      textBlock = """
+          # catalogName
+          AdventureWorks2022
+          """,
+      delimiter = '|')
+  void check(@Nullable String catalogName) throws Exception {
+    // given
+    var precondition = new ConnectionCatalogPrecondition();
+    precondition.setCatalogName(catalogName);
+    try (var database =
+        DatabaseBuilder.of(MarkitectMssqlDatabase.class)
+            .withOfflineConnection(
+                ocb -> ocb.withSnapshot("snapshots/mssql/AdventureWorks2022.json"))
+            .build()) {
+      assertThat(database.getDefaultCatalogName()).isEqualTo("AdventureWorks2022");
+
+      // when
+      var thrown = catchThrowable(() -> precondition.check(database, null, null, null));
+
+      // then
+      assertThat(thrown).isNull();
+    }
+  }
+
+  @ParameterizedTest
+  @CsvSource(textBlock = """
+          # catalogName
+          cat1
+          """, delimiter = '|')
+  void checkThrowsPreconditionErrorException(@Nullable String catalogName)
+      throws DatabaseException {
+    // given
+    var precondition = new ConnectionCatalogPrecondition();
+    precondition.setCatalogName(catalogName);
+    try (var database = DatabaseBuilder.of(MarkitectMssqlDatabase.class).build()) {
+
+      // when
+      var thrown = catchThrowable(() -> precondition.check(database, null, null, null));
+
+      // then
+      assertThat(thrown).isInstanceOf(PreconditionErrorException.class);
+    }
+  }
+
+  @ParameterizedTest
+  @CsvSource(
+      textBlock =
+          """
+          # catalogName | expectedMessage
+          master        | Connection catalog precondition failed: expected master, was AdventureWorks2022
+          cat1          | Connection catalog precondition failed: expected cat1, was AdventureWorks2022
+          """,
+      delimiter = '|')
+  void checkThrowsPreconditionFailedException(@Nullable String catalogName, String expectedMessage)
+      throws DatabaseException {
+    // given
+    var precondition = new ConnectionCatalogPrecondition();
+    precondition.setCatalogName(catalogName);
+    try (var database =
+        DatabaseBuilder.of(MarkitectMssqlDatabase.class)
+            .withOfflineConnection(
+                ocb -> ocb.withSnapshot("snapshots/mssql/AdventureWorks2022.json"))
+            .build()) {
+      assertThat(database.getDefaultCatalogName()).isEqualTo("AdventureWorks2022");
+
+      // when
+      var thrown = catchThrowable(() -> precondition.check(database, null, null, null));
+
+      // then
+      assertThat(thrown)
+          .asInstanceOf(type(PreconditionFailedException.class))
+          .extracting(PreconditionFailedException::getFailedPreconditions)
+          .asInstanceOf(list(FailedPrecondition.class))
+          .extracting(FailedPrecondition::getMessage)
+          .containsExactly(expectedMessage);
+    }
+  }
+}
