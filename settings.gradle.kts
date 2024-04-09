67915fd89bd70046104a0b5d41b6c1d3f1e75ad9
Mark Chesney <mches@users.noreply.github.com>
2024-04-09 14:58:22 -0700

    Migrate to Develocity plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise") version "3.17.1"
+    id("com.gradle.develocity") version "3.17.1"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
@@ -17,12 +17,13 @@ dependencyResolutionManagement {
     }
 }
 
-gradleEnterprise {
-    if (providers.environmentVariable("CI").isPresent) {
-        buildScan {
-            termsOfServiceUrl = "https://gradle.com/terms-of-service"
-            termsOfServiceAgree = "yes"
+develocity {
+    buildScan {
+        if (providers.environmentVariable("CI").isPresent) {
+            termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+            termsOfUseAgree.set("yes")
         }
+        publishing.onlyIf { false }
     }
 }
 
