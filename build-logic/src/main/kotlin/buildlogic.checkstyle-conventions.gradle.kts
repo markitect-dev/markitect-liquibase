dfdcd714477444f4892e5a8a9ddd016c08220a68
Mark Chesney <mches@users.noreply.github.com>
2024-05-18 13:37:26 -0700

    Remove temporary workaround

--- build-logic/src/main/kotlin/buildlogic.checkstyle-conventions.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.checkstyle-conventions.gradle.kts
@@ -2,13 +2,6 @@ plugins {
     checkstyle
 }
 
-configurations.checkstyle {
-    resolutionStrategy.dependencySubstitution {
-        substitute(module("com.google.collections:google-collections"))
-            .using(module(libs.com.google.guava.guava.get().toString()))
-    }
-}
-
 checkstyle {
     toolVersion = libs.versions.checkstyle.get()
 }
