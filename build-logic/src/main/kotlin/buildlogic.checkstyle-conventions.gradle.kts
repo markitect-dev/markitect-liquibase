ff110e42923b1e603bdb330248202a9158188b84
Mark Chesney <mches@users.noreply.github.com>
2024-08-25 16:22:47 -0700

    Refine CheckStyle, CheckStyle plugin config

--- build-logic/src/main/kotlin/buildlogic.checkstyle-conventions.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.checkstyle-conventions.gradle.kts
@@ -4,4 +4,16 @@ plugins {
 
 checkstyle {
     toolVersion = libs.versions.checkstyle.get()
+    configProperties =
+        mapOf(
+            "org.checkstyle.google.suppressionfilter.config" to
+                configDirectory.file("suppressions.xml").get().asFile.path,
+            "org.checkstyle.google.suppressionxpathfilter.config" to
+                configDirectory.file("xpath-suppressions.xml").get().asFile.path,
+        )
+    maxWarnings = 0
+}
+
+tasks.withType<Checkstyle>().configureEach {
+    group = "verification"
 }
